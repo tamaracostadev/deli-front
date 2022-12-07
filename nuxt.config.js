42c9e36680c36779aa13e2f20be1a28295a12310
tamara-costa <tamara@pennacorp.com.br>
2022-12-07 01:55:55 -0300

    Revert "Custos e depreciação vue"
    This reverts commit d1a7e8888912e03472ec25233078826fe70ea354.

--- nuxt.config.js
+++ nuxt.config.js
@@ -116,10 +116,6 @@ export default {
 			laravelSanctum: {
 				provider: 'laravel/sanctum',
 				url: '/back',
-				token: {
-					property: 'token',
-				},
-				tokenRequired: true,
 				endpoints: {
 					// (optional) If set, we send a get request to this endpoint before login
 					csrf: {
